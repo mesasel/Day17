ef7fb0427ed7ab13b00674ec5f79e67bd051bafb
mesasel <80010827+mesasel@users.noreply.github.com>
2021-03-25 14:07:09 +0100

    Updated Day17

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,13 +1,8 @@
-const array1 = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
+const array1 = ["MON", "TUE", "WED", "THU", "FRI"];
 
 function main(arr) {
-    // Only change code below this line
-
-    let array2 = [...array1]; // Change this line
-
-    // Only change code above this line
-    return array2;
+  let array2 = [...arr];
+  return array2;
 }
-
 console.log(main(array1));
 module.exports = main;

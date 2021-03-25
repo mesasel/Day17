6e293720a669c3a66dc2820726ff9aae3fe7a72b
mesasel <80010827+mesasel@users.noreply.github.com>
2021-03-25 14:07:44 +0100

    Updated Day17

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,17 +1,8 @@
-const MY_CAR = {
-  wheels: 4,
-  cylinders: 4,
-  size: 2.2,
-};
+const MY_CAR = { wheels: 4, cylinders: 4, size: 2.2 };
 
 function main(myCar) {
-  // Only change code below this line
-
-  const { cylinders, size } = MY_CAR;
-
+  const { cylinders, size } = myCar;
   return { cylinders, size };
-  // Only change code above this line
 }
-
 console.log(main(MY_CAR));
 module.exports = main;
